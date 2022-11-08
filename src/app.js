46e3bfe460aa05149141c2bd21da0e2f0f05d6a5
Yan5577 <zuoyan775@gmai.com>
2022-11-08 11:32:33 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
 // sample usage:
 // node src/app.js --action=add --m1 "the longest day" --m2 casablanca --m3 "spiderman"
 
+const yargs = require('yargs');
+const Movie = require("./utils")
 
-const yargs = require("yargs");
-const input = yargs(process.argv);
-const movie = require("./utils");
-const params = input.argv;
+const input = yargs.argv;
 
-movie(params.action, params.m1, params.m2, params.m3);
+function app(yargsObject) {
+    if (yargsObject.add) {
+        const newMovie = new Movie(yargsObject.title, yargsObject.actor)
+        newMovie.add();
+    } else if (yargsObject.multiadd) {
+        const newMovie1 = new Movie(yargsObject.title1, yargsObject.actor1)
+        newMovie1.add();
+        const newMovie2 = new Movie(yargsObject.title2, yargsObject.actor2)
+        newMovie2.add();
+        const newMovie3 = new Movie(yargsObject.title3, yargsObject.actor3)
+        newMovie3.add();
+    }
+}
+
+app(input);
+
+
+
+
+
+
+
+
+// const yargs = require("yargs");
+// const input = yargs(process.argv);
+// const movie = require("./utils");
+// const params = input.argv;
+
+// movie(params.action, params.m1, params.m2, params.m3);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,38 @@
-const yargs = require("yargs");
 
-function movie(action, m1, m2, m3) {
-  switch (action) {
-    case "add":
-      const movie = [m1, m2, m3];
-      console.table(movie);
-      break;
-    default:
-      console.log("Command not recognised");
-  }
+const movieArray = [];
+
+class Movie {
+    constructor (title, actor="Not Specified") {
+        this.title = title;
+        this.actor = actor;
+    }
+    add() {
+        movieArray.push(this);
+        console.table(movieArray);
+    }
 }
-module.exports = movie;
+
+module.exports = Movie;
+
+
+
+
+
+
+
+
+
+
+// const yargs = require("yargs");
+
+// function movie(action, m1, m2, m3) {
+//   switch (action) {
+//     case "add":
+//       const movie = [m1, m2, m3];
+//       console.table(movie);
+//       break;
+//     default:
+//       console.log("Command not recognised");
+//   }
+// }
+// module.exports = movie;
